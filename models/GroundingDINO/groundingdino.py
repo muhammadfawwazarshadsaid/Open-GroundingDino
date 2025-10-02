bbdf7300ba269a87a517c62f35710e9e02b851bb
Arshad <arshad@MacBook-Pro-Arshad.local>
2025-10-03 01:26:03 +0700

    def forward(self, *, samples: NestedTensor, targets: Optional[List] = None, captions: Optional[List] = None, **kwargs):

--- models/GroundingDINO/groundingdino.py
+++ models/GroundingDINO/groundingdino.py
@@ -213,7 +213,7 @@ class GroundingDINO(nn.Module):
     def init_ref_points(self, use_num_queries):
         self.refpoint_embed = nn.Embedding(use_num_queries, self.query_dim)
 
-    def forward(self, *, samples: NestedTensor, targets: List = None, **kw):
+    def forward(self, *, samples: NestedTensor, targets: Optional[List] = None, captions: Optional[List] = None, **kwargs):
         """The forward expects a NestedTensor, which consists of:
            - samples.tensor: batched images, of shape [batch_size x 3 x H x W]
            - samples.mask: a binary mask of shape [batch_size x H x W], containing 1 on padded pixels
