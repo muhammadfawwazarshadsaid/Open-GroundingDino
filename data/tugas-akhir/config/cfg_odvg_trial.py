51ec85d1af814a7b0812d246ad0cb054839944e6
Arshad <arshad@MacBook-Pro-Arshad.local>
2025-10-04 18:52:33 +0700

    trial

--- data/tugas-akhir/config/cfg_odvg_trial.py
+++ data/tugas-akhir/config/cfg_odvg_trial.py
@@ -113,5 +113,12 @@ use_ema = False
 ema_decay = 0.9997
 ema_epoch = 0
 use_detached_boxes_dec_out = False
-use_coco_eval = True
+# use_coco_eval = True
+use_coco_eval = False
+label_list = [
+    "Auxiliary", "Base Plate", "Box", "Connection Power Supply", 
+    "Door Handle Drawer", "Drawer Stopper", "Front Plate", "Handle Drawer",
+    "Index Mechanism", "Locking Mechanism", "Mounting Component", 
+    "Push Button Index Mechanism", "Roda Drawer", "Support Outgoing", "Top Plate"
+]
 dn_scalar = 100
